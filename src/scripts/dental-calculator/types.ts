f46577f903556acc480e06dcd6799115fc97b9f8
backofnapkin <brett.lindenberg@gmail.com>
2024-12-17 16:06:51 -0700

    Create types.ts

---
+++ src/scripts/dental-calculator/types.ts
@@ -0,0 +1,36 @@
+export interface Employee {
+  title: string;
+  monthlySalary: number;
+}
+
+export interface OverheadCost {
+  name: string;
+  monthlyAmount: number;
+}
+
+export interface CalculatorInputs {
+  practiceName: string;
+  dentalChairs: number;
+  patientsPerChair: number;
+  revenuePerPatient: number;
+  daysPerWeek: number;
+  startupCosts: number;
+  employees: Employee[];
+  overheadCosts: OverheadCost[];
+}
+
+export interface FinancialProjections {
+  monthlyRevenue: number;
+  annualRevenue: number;
+  monthlyOverhead: number;
+  annualOverhead: number;
+  netIncome: number;
+  profitMargin: number;
+  yearlyProjections: number[];
+}
+
+export interface Scenarios {
+  current: FinancialProjections;
+  bestCase: FinancialProjections;
+  worstCase: FinancialProjections;
+}

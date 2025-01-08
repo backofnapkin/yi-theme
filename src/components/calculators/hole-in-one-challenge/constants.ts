6bf946bf4f653964f9423b1622a87905c2d95ed6
backofnapkin <brett.lindenberg@gmail.com>
2025-01-08 16:35:25 -0700

    Create constants.ts

---
+++ src/components/calculators/hole-in-one-challenge/constants.ts
@@ -0,0 +1,27 @@
+export const GOLFER_PROFILES = [
+    {
+      type: 'Pro',
+      odds: 5000,
+      description: 'Pro golfers have a 1 in 5,000 chance'
+    },
+    {
+      type: 'Good',
+      odds: 10000,
+      description: 'Good golfers have a 1 in 10,000 chance'
+    },
+    {
+      type: 'Amateur',
+      odds: 12000,
+      description: 'Amateur golfer has a 1 in 12,000 chance'
+    },
+    {
+      type: 'Casual',
+      odds: 20000,
+      description: 'Casual golfer has a 1 in 20,000 chance'
+    },
+    {
+      type: 'Terrible',
+      odds: 25000,
+      description: 'Terrible golfers have a 1 in 25,000 chance'
+    }
+  ];

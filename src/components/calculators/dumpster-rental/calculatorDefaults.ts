0dfee23f1f2ba049976fee4d3d4823bfac7c7c39
backofnapkin <brett.lindenberg@gmail.com>
2025-02-03 08:57:21 -0700

    Create calculatorDefaults.ts

---
+++ src/components/calculators/dumpster-rental/calculatorDefaults.ts
@@ -0,0 +1,26 @@
+export const defaultCalculatorState = {
+    businessName: "Dave's Dumpster Rentals",
+    // Startup Costs
+    rollOffTrucks: 1,
+    rollOffTruckCost: 95000,
+    dumpsterCount: 5,
+    dumpsterCost: 5000,
+    rentalPricePerWeek: 384,
+    rentalsPerMonth: 4,
+    additionalStartupCosts: [],
+    
+    // Operational Costs
+    maintenanceCostPerYear: 300,
+    fullTimeDrivers: 1,
+    driverWagePerHour: 25,
+    fuelCostPerMonth: 500,
+    marketingCostsPerMonth: 500,
+    insuranceCostPerMonth: 300,
+    loanRepaymentPerMonth: 0,
+    softwareCostsPerMonth: 99,
+    additionalOperationalCosts: [],
+    
+    // Additional Revenue
+    junkRemovalRevenue: 1500,
+    additionalServices: []
+  };
